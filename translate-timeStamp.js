22ef90b04d08af2f7e2781896beba63a3dabb3d8
Majid Valipour <majidvp@chromium.org>
2016-01-29 11:35:14 -0500

    Formatting

--- translate-timeStamp.js
+++ translate-timeStamp.js
@@ -2,32 +2,33 @@
 // Copyright (c) 2016 Majid Valipour
 
 // Browsers (and the DOM spec) are in the process of moving the event timestamp
-// from being a DOMTimeStamp (mostly relative to Date.now()) to being
-// a DOMHighResTimeStamp (always relative to performance.now()).
+// from being a DOMTimeStamp (mostly relative to Date.now()) to being a
+// DOMHighResTimeStamp (always relative to performance.now()).
 
 // A simple utility function that translates an Event.timeStamp to a
-// DOMHighResTimeStamp that can be compare with performance.now().
-// This becomes a no-op for browsers that provide high resolution
-// event timestamp.
+// DOMHighResTimeStamp that can be compare with performance.now(). This becomes
+// a no-op for browsers that provide high resolution event timestamp.
 //
 // Usage example:
 // var latency = performance.now() - getHighResTimeStamp(event);
-getHighResTimeStamp = (function(){
+getHighResTimeStamp = (function() {
   var dateNowAtLoad = Date.now();
-  // The offset value to system startup clock that is being used
-  // by Firefox as timebase for all input events.
+  // The offset value to system startup clock that is being used by Firefox as
+  // timebase for all input events.
   var systemStartupClockOffset;
 
   // Optimization: Check if this browser is already using DOMHighResTimeStamp
   // for event timestamp and return a no-op function in that case.
   var testTimeStamp = !!window.CustomEvent ? new CustomEvent('test').timeStamp : document.createEvent('KeyboardEvent').timeStamp;
   if (testTimeStamp <= performance.now())
-    return function(event) { return event.timeStamp; };
+    return function(event) {
+      return event.timeStamp;
+    };
 
   function getTimebaseOffset(event) {
     if (event.timeStamp >= dateNowAtLoad) {
       // Timestamp uses Unix epoch timebase
-      return - performance.timing.navigationStart;
+      return -performance.timing.navigationStart;
     } else {
       var now = performance.now();
       if (event.timeStamp <= now) {
@@ -40,10 +41,10 @@ getHighResTimeStamp = (function(){
         //   2. Unix epoch for all user script constructed events.
         // (1) is handled here but (2) is handled above.
 
-        // Compute system startup clock offset which is the difference between current
-        // time in high resolution clock vs system startup clock.
-        // We use the passed in event timestamp as an approximation of the current
-        // time in the  system startup clock.
+        // Compute system startup clock offset which is the difference between
+        // current time in high resolution clock vs system startup clock. We use
+        // the passed in event timestamp as an approximation of the current time
+        // in the system startup clock.
         return systemStartupClockOffset || (systemStartupClockOffset = Math.round(now - event.timeStamp));
       }
     }
@@ -58,4 +59,3 @@ getHighResTimeStamp = (function(){
     return event.timeStamp + getTimebaseOffset(event);
   }
 })();
-
